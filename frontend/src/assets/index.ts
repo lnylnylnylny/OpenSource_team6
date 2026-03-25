6d2b6f361955cf9e3e18e8f4a5c8e7caa4b38365
나영 <160034314+lnylnylnylny@users.noreply.github.com>
2026-03-25 09:53:23 +0900

    feat: asset 주입

---
+++ frontend/src/assets/index.ts
@@ -0,0 +1,10 @@
+// NavBar
+
+export { default as home } from "./icons/house.svg";
+export { default as quiz } from "./icons/quiz.svg";
+export { default as game } from "./icons/game.svg";
+export { default as mypage } from "./icons/user.svg";
+export { default as home_active } from "./icons/house_active.svg";
+export { default as quiz_active } from "./icons/quiz_active.svg";
+export { default as game_active } from "./icons/game_active.svg";
+export { default as mypage_active } from "./icons/user_active.svg";
