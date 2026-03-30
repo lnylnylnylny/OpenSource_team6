31661f33ae3f577958c8bcd901f5223d0afd6aa6
Jaehee Lee <55607202+ij5@users.noreply.github.com>
2026-03-30 13:55:26 +0900

    Revert "Feat/quiz"

--- frontend/src/pages/Quiz.tsx
+++ frontend/src/pages/Quiz.tsx
@@ -1,13 +1,8 @@
 import { NavBar } from "../components/NavBar";
-import { QuizCard } from "../components/QuizCard"; 
 
 export const Quiz = () => {
   return (
-    <div className="min-h-screen bg-[#F2F2F6] flex flex-col items-center">
-      <main className="flex-1 w-full max-w-[393px]">
-        {/* 퀴즈 카드 띄우기 */}
-        <QuizCard />
-      </main>
+    <div>
       <NavBar />
     </div>
   );
