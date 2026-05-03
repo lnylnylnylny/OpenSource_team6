da6df6e5905a5d460055b4ea6192437f319dea41
나영 <160034314+lnylnylnylny@users.noreply.github.com>
2026-05-04 00:00:35 +0900

    feat: 마이페이지 기본 프로필 이미지 적용

--- frontend/src/pages/Mypage.tsx
+++ frontend/src/pages/Mypage.tsx
@@ -1,27 +1,23 @@
+import { HoldingsTable } from "../components/HoldingStable";
+import { InvestmentSummary } from "../components/InvestmentSummary";
 import { NavBar } from "../components/NavBar";
 import { useAuthStore } from "../store/authStore";
+import { profile } from "../assets";
 
 export const Mypage = () => {
   const user = useAuthStore((state) => state.user);
   if (!user) return null;
 
-  const initial = user.nickname[0] ?? "";
   return (
     <div>
       <div className=" px-5 pt-10 pb-6 ">
         <div className="flex items-center gap-4">
           <div className="w-14 h-14 rounded-full flex items-center justify-center shrink-0 overflow-hidden">
-            {user.profile_image ? (
-              <img
-                src={user.profile_image}
-                alt="프로필"
-                className="w-full h-full object-cover"
-              />
-            ) : (
-              <span className="text-xl font-medium text-[#0F6E56]">
-                {initial}
-              </span>
-            )}
+            <img
+              src={user.profile_image || profile}
+              alt="프로필"
+              className="w-full h-full object-cover"
+            />
           </div>
 
           <div className="flex flex-col gap-1.5">
@@ -32,6 +28,9 @@ export const Mypage = () => {
           </div>
         </div>
       </div>
+
+      <InvestmentSummary />
+      <HoldingsTable />
       <NavBar />
     </div>
   );
